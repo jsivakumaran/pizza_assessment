8c0f452bc7ed04745b4b0e7a839629a8be485134
jsivakumaran <nayagan@gmail.com>
2015-02-20 09:41:52 -0800

    Add full specs for pizza.order

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
 var Pizza = {
+  type: "",
+  size: 0,
+  slices: 0,
   order: function(number){
-    return 12;
+    var inches = number;
+    if(isNaN(inches)){
+      return 0;
+    }else if ((inches<=8) && (inches>=1)){
+      return 4;
+    }else if ((inches<=12) && (inches>=8)){
+
+      return 8;
+    }else if ((inches<=16) && (inches>=12)){
+
+      return 12;
+    }else {
+      return 0;
+    }
   }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,20 @@
 describe("Pizza", function(){
-  it("returns the amount of slices when given the inches to order()", function(){
-    var instance_pizza = Object.create(Pizza);
-    var slices = instance_pizza.order(36);
-    expect(slices).to.equal(12);
-  });
 
+  describe("order", function(){
+    it("returns the amount of slices when given a number from 1-16", function(){
+      var instance_pizza = Object.create(Pizza);
+      var slices = instance_pizza.order(13);
+      expect(slices).to.equal(12);
+    });
+    it("returns 0 when given a number that is outside the range of 1-16", function(){
+      var instance_pizza = Object.create(Pizza);
+      var slices = instance_pizza.order(30);
+      expect(slices).to.equal(0);
+    });
+    it("returns 0 when a string or something that is not a number is entered", function(){
+      var instance_pizza = Object.create(Pizza);
+      var slices = instance_pizza.order("jay");
+      expect(slices).to.equal(0);
+    });
+  });
 });
