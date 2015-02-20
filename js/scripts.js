06dfdd67a5bcdf60101163307585b12233f0c50d
jsivakumaran <nayagan@gmail.com>
2015-02-20 09:52:34 -0800

    Fix return values from order()

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,20 @@ var Pizza = {
   order: function(number){
     var inches = number;
     if(isNaN(inches)){
-      return 0;
+      this.slices=0;
     }else if ((inches<=8) && (inches>=1)){
-      return 4;
+      this.slices=4;
     }else if ((inches<=12) && (inches>=8)){
-
-      return 8;
+      this.slices=8;
     }else if ((inches<=16) && (inches>=12)){
-
-      return 12;
+      this.slices=12;
     }else {
-      return 0;
+      this.slices=0;
     }
   }
 }
+
+$(document).ready(function(){
+  var pizza = Object.create(Pizza);
+  
+});
--- spec/specs.js
+++ spec/specs.js
@@ -4,17 +4,17 @@ describe("Pizza", function(){
     it("returns the amount of slices when given a number from 1-16", function(){
       var instance_pizza = Object.create(Pizza);
       var slices = instance_pizza.order(13);
-      expect(slices).to.equal(12);
+      expect(instance_pizza.slices).to.equal(12);
     });
     it("returns 0 when given a number that is outside the range of 1-16", function(){
       var instance_pizza = Object.create(Pizza);
       var slices = instance_pizza.order(30);
-      expect(slices).to.equal(0);
+      expect(instance_pizza.slices).to.equal(0);
     });
     it("returns 0 when a string or something that is not a number is entered", function(){
       var instance_pizza = Object.create(Pizza);
       var slices = instance_pizza.order("jay");
-      expect(slices).to.equal(0);
+      expect(instance_pizza.slices).to.equal(0);
     });
   });
 });
